f5405f7e51d266102bdcbc498573a09cd7d78c6d
Nontakon Kanjhanopas (นนทกร กาญจโนภาศ) <6610110137@email.psu.ac.th>
2025-02-18 21:12:10 +0700

    create content

---
+++ backend/src/api/lotto6/controllers/lotto6.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * lotto6 controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::lotto6.lotto6');
