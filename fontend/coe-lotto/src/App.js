42f08e7a3ace3a4898c764ef5bbaae1286bc2b67
althos <jukrachai3146@gmail.com>
2025-02-21 02:05:46 +0700

    fontend

--- fontend/coe-lotto/src/App.js
+++ fontend/coe-lotto/src/App.js
@@ -1,12 +1,23 @@
-
-import NavBar from './components/NavBar';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <main>
-        <h1 className="text-center text-2xl font-bold mt-10">Welcome to CoE Lotto</h1>
-      </main>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
