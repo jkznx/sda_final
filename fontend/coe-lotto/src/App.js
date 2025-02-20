d826229bce33c12d16b6a18bb2fc7f55ea13358b
althos <jukrachai3146@gmail.com>
2025-02-21 01:40:00 +0700

    fontend

---
+++ fontend/coe-lotto/src/App.js
@@ -0,0 +1,14 @@
+
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <div>
+      <main>
+        <h1 className="text-center text-2xl font-bold mt-10">Welcome to CoE Lotto</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
