89bb65e4af1147a715ccab2177c761e40ed46d30
Nontakon Kanjhanopas (นนทกร กาญจโนภาศ) <6610110137@email.psu.ac.th>
2025-03-02 17:10:36 +0700

    fix

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'yourGeneratedSecretKey')
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'yourGeneratedSaltHere'),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'yourGeneratedSalt'), 
     },
   },
   flags: {
